848308cd23f088a9b5e5cb60fde82bf1489f7827
ouyangessen <406760081@qq.com>
2018-11-29 16:06:09 +0000

    stautses

--- app/Models/User.php
+++ app/Models/User.php
@@ -34,4 +34,8 @@ class User extends Authenticatable
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
 
+    public function statuses(){
+        return $this->hasMany(Status::class);
+    }
+
 }
